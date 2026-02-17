1e373a494608146c2d814c987e886509953d653e
Rom <whiteromka@yandex.ru>
2026-02-17 23:01:18 +0300

    JS для блока модули курса

--- resources/views/components/cyber/items.blade.php
+++ resources/views/components/cyber/items.blade.php
@@ -1,4 +1,3 @@
-<div class="" style="">
 <div class="container">
     <h2 class="h2-common">
         {{--        <span>0.01</span> <br>--}}
@@ -14,9 +13,12 @@
         <span> [][][]== ===</span>
     </div>
 </div>
-
-<div class="container-fluid px-0 bg-purple">
+<div class="container-fluid  bg-purple px-0">
     <br>
+</div>
+
+<div class="container-fluid px-0 bg-purple_  wrapper-starfield ">
+    <canvas id="starfield"></canvas>
     <div class="container-fluid px-0 signal-light">
         <div class="container cy-items-container" >
             <div class="row">
@@ -84,9 +86,10 @@
             </div>
         </div>
     </div>
-    <br>
-    <br>
 </div>
+
+<div class="container-fluid  bg-purple px-0">
+    <br>
 </div>
 <div class="container-fluid bottom-ark bg-purple px-0">
     <div class="streaks">
@@ -102,6 +105,94 @@
 
 @push('scripts')
 <script>
+    // Для создания звездного неба
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('!!!');
+        const canvas = document.getElementById('starfield');
+        const ctx = canvas.getContext('2d');
+        const container = canvas.parentElement;
+
+        function resize() {
+            canvas.width = container.clientWidth;
+            canvas.height = container.clientHeight;
+        }
+
+        resize();
+        window.addEventListener('resize', resize);
+
+        // Настройки
+        const config = {
+            stars1px: 120,   // количество маленьких звезд
+            stars2px: 60,    // количество больших звезд
+            speed1px: 0.04,   // скорость маленьких
+            speed2px: 0.2    // скорость больших
+        };
+        const stars = [];
+
+        class Star {
+            constructor(size, baseSpeed) {
+                this.size = size;
+                this.baseSpeed = baseSpeed;
+                this.reset(false);
+            }
+
+            reset(fromBottom = false) {
+                this.x = Math.random() * canvas.width;
+
+                this.y = fromBottom
+                    ? canvas.height + Math.random() * canvas.height * 0.3
+                    : Math.random() * canvas.height;
+
+                // индивидуальная скорость
+                this.speed = this.baseSpeed * (0.5 + Math.random());
+                // микродвижение вбок
+                this.drift = (Math.random() - 0.5) * 0.05;
+            }
+
+            update() {
+                this.y -= this.speed;
+                this.x += this.drift;
+
+                // если улетела вверх или за край по X
+                if (
+                    this.y < -this.size ||
+                    this.x < -10 ||
+                    this.x > canvas.width + 10
+                ) {
+                    this.reset(true);
+                }
+            }
+
+            draw() {
+                ctx.fillStyle = '#fff';
+                ctx.fillRect(this.x, this.y, this.size, this.size);
+            }
+        }
+
+        // Создание звезд
+        for (let i = 0; i < config.stars1px; i++) {
+            stars.push(new Star(1, config.speed1px));
+        }
+
+        for (let i = 0; i < config.stars2px; i++) {
+            stars.push(new Star(2, config.speed2px));
+        }
+
+        // Анимация
+        function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (const star of stars) {
+                star.update();
+                star.draw();
+            }
+            requestAnimationFrame(animate);
+        }
+        animate();
+    });
+</script>
+
+<script>
+    // Для управления анимациями при наведении на модуль
     document.addEventListener('DOMContentLoaded', () => {
         const cyItems = document.querySelectorAll('.cy-item');
 
